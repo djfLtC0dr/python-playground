6e8f2ab8f2ddf7313a982844dd44473cfa2a743d
djfLtC0dr <fawcettdj1@gmail.com>
2022-07-14 22:08:52 -0400

    hw2 problem 2-3

--- DASC512/hw2.py
+++ DASC512/hw2.py
@@ -5,10 +5,8 @@ import pandas as pd
 import scipy.stats as stats
 import seaborn as sns
 import statistics as stat
-from scipy.stats import binom
 
 '''Problem 1'''
-# number of samples, probability of contamination
 x = 2
 x_minus_1 = x - 1
 p5 = (0.23)*(0.77)**x_minus_1
@@ -17,9 +15,26 @@ p5 = (0.23)*(0.77)**x_minus_1
 # print(x.pmf(1)) # P(x = 1)
 # print(x.pmf(5)) # P(x = 5)
 
+'''Problem 2'''
 x, p, n = 15, 0.8, 20
-prob = stats.binom.cdf(x, n, p)
-print(prob)
-# np.allclose(x, stats.binom.ppf(prob, n, p))
+cum_prob = stats.binom.cdf(x, n, p)
+print(cum_prob)
+print(1 - cum_prob)
+exact_prob = stats.binom.pmf(x, n, p)
+print(exact_prob)
+# obtaining the mean and variance 
+mean, var = stats.binom.stats(n, p)
+print(mean)
 
+'''Problem 3'''
+x, mu = 3, 5
+prob_3 = stats.poisson.cdf(x, mu)
+print(prob_3)
 
+'''Problem 4'''
+mean = 850,000
+stdev = 170,000
+R = 1,000,000
+
+'''Problem 5'''
+mu = 10
