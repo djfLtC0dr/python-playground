fe5fd070b5d7c122e0d3196508b3a11bb0e5a638
DanielFawcett1 <>
2022-07-21 21:00:37 +0000

    hw3 p6

--- DASC512/hw3.py
+++ DASC512/hw3.py
@@ -10,6 +10,7 @@ import statistics as stat
 from pathlib import Path
 from statsmodels.stats import power
 import statsmodels.api as sm
+from statsmodels.stats import weightstats as stests
 
 '''Problem 1a Right-Tail Test'''
 placebo = (105, 119, 100, 97, 96, 101, 94, 95, 98)
@@ -138,3 +139,69 @@ E = 14.9 - 14.8
 z_crit = stats.norm.ppf(1 - alpha)
 n = ((z_crit * sigma) / E)**2
 print(n)
+
+'''Problem 6'''
+df_bavg = pd.read_csv("BattingAverages.csv", sep = ',')
+# Clean-up
+df_bavg.drop('Unnamed: 6', axis=1, inplace=True)
+# print(df_bavg.head())
+size = len(df_bavg.index)
+# print('bavg length: ' + str(size))
+mu, sigma = np.mean(df_bavg['BattingAvg']), np.std(df_bavg['BattingAvg'])
+# print('mu bat avg = ', mu)
+means = np.random.normal(mu, sigma, size)
+fig, (ax_box, ax_hist) = plt.subplots(2, sharex=True, 
+            gridspec_kw={"height_ratios": (.15, .85)}, figsize = (12, 8))
+fig.tight_layout(pad = 3)
+fig.suptitle('Uniform CLT Boxplot & Histogram')
+# Create the bins and histogram
+sns.histplot(data=means, ax=ax_hist, color = 'green', kde=True, stat="count", linewidth=0)
+
+# Use scipy.stats implementation of the normal pdf
+# Plot the distribution curve
+x = np.linspace(0, 0.5, num=size)
+plt.plot(x, stats.norm.pdf(x, mu, sigma))
+sns.boxplot(data=means, ax=ax_box, color = 'darkgreen')
+#remember chi square didn't look normal until ~50-100
+fig, ax = plt.subplots(figsize=(6,4))
+fig.suptitle('QQ-Plot')
+fig.tight_layout(pad=3)
+pp = sm.ProbPlot(np.array(means), stats.norm, fit=True)
+qq = pp.qqplot(marker='.', ax=ax, markerfacecolor='darkorange', markeredgecolor='darkorange', alpha=0.8)
+sm.qqline(qq.axes[0], line='45', fmt='k--')
+# plt.show()
+stat, pval = stats.normaltest(means)
+print('Statistics=%.3f, p=%.3f' % (stat, pval))
+# interpret
+alpha = 0.05
+if pval > alpha:
+	print('Sample looks Normal (fail to reject H0)')
+else:
+	print('Sample does not look Normal (reject H0)')
+
+df_bavg['BattingAvg'].describe()
+zstat, pval = stests.ztest(df_bavg['BattingAvg'], x2=None, value=0.265)
+print('zstat=%.3f, p=%.3f' % (zstat, pval))
+if pval < alpha:
+  print("Null hyphothesis rejected , Alternative hyphothesis accepted")
+else:
+  print("Null hyphothesis accepted , Alternative hyphothesis rejected")
+
+# perform power analysis
+analysis = power.TTestIndPower()
+power = analysis.solve_power(power=None, effect_size=1, alpha=0.05, nobs1=10, ratio=1.0)
+print('Power: %.3f' % power)
+
+df_ba_nl = df_bavg.loc[df_bavg['League'] == 'National League']['BattingAvg']
+df_ba_al = df_bavg.loc[df_bavg['League'] == 'American League']['BattingAvg']
+print(np.std(df_ba_nl, ddof=1), np.std(df_ba_al, ddof=1))
+# out => 0.033159408525780996 0.03484792169158821
+# = stdev so need to use equal_var = True testing for zero order doesn't matter
+t_stat, p_val = stats.ttest_ind(df_ba_nl, df_ba_al, equal_var = True)
+print('t_stat=%.3f, p=%.3f' % (stat, p_val))
+# interpret
+alpha = 0.05
+if p_val > alpha:
+	print('Same distributions (fail to reject H0)')
+else:
+	print('Different distributions (reject H0)')
