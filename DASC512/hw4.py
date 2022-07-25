dc81595f7b9ce8ce91db402aa8615d3caf0eb379
djfLtC0dr <fawcettdj1@gmail.com>
2022-07-25 11:04:45 -0400

    hw4 setup

---
+++ DASC512/hw4.py
@@ -0,0 +1,11 @@
+'''Dan Fawcett DASC 512 Homework 4'''
+#Standard Import List
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.stats as stats
+import seaborn as sns
+import statistics as stat
+from statsmodels.stats import power
+import statsmodels.api as sm
+from statsmodels.stats import weightstats as stests
