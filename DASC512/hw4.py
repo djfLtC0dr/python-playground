2e5d2ec09d2196bebe11a340d03e85fec189478d
djfLtC0dr <fawcettdj1@gmail.com>
2022-07-26 21:49:53 -0400

    hw4 p2 start

--- DASC512/hw4.py
+++ DASC512/hw4.py
@@ -10,16 +10,17 @@ import statsmodels.api as sm
 from statsmodels.formula.api import ols
 from statsmodels.stats.anova import anova_lm
 from statsmodels.graphics.factorplots import interaction_plot
-import csv
 
 '''Problem 1'''
 # A study was designed to determine if a persons essay quality or attractiveness 
 # played a role in the grading of essays. Column 1 in the data is essay quality 
-# (rated good=1,poor=2), the second column is student attractiveness (1=attractive, 2=control, 3=unattractive). And the third column is the response (essay score).
-# Construct an interaction plot and the full anova table (row for treatment as well as each factor). The .dat file is a tab delimited text file
+# (rated good=1,poor=2), the second column is student attractiveness 
+# (1=attractive, 2=control, 3=unattractive). And the third column is the response (essay score).
+# Construct an interaction plot and the full anova table (row for treatment as well as each factor). 
+# The .dat file is a tab delimited text file
 
 # Import data file into a dataframe
-txt_file = r"halo1.dat"
+txt_file = "halo1.dat"
 # essay quality = eq, student attractiveness = sa, essay score = score
 COLUMN_NAMES=['eq','sa','score']
 
@@ -31,7 +32,8 @@ with open(txt_file) as f:
 #Create Interaction Plot
 fig, ax = plt.subplots(figsize=(12,6))
 fig = interaction_plot(data['eq'], data['sa'], data['score'],
-                       colors=['green', 'yellow', 'blue'], markers=['1', '2', '3'], ms=10, ax=ax)
+                       colors=['green', 'red', 'blue'], markers=['1', '2', '3'], ms=10, ax=ax)
+
 #Calculate Degrees of Freedom
 n=len(data.score)
 df_a = len(data['eq'].unique())-1
@@ -43,17 +45,17 @@ df_t = df_a+df_b+df_ab
 #Calculate Sum of Squares
 grand_mean = data['score'].mean()
 
-ssa = sum([(data[data['eq'] ==l].score.mean()-grand_mean)**2 for l in data['eq']])
+ssa = sum([(data[data['eq'] ==l]['score'].mean()-grand_mean)**2 for l in data['eq']])
 
-ssb = sum([(data[data['sa'] ==l].score.mean()-grand_mean)**2 for l in data['sa']])
+ssb = sum([(data[data['sa'] ==l]['score'].mean()-grand_mean)**2 for l in data['sa']])
 
 sstotal = sum((data['score'] - grand_mean)**2)
 
 eq1 = data[data['eq'] == 1]
 eq2 = data[data['eq'] == 2]
-eq1_sa_means = [eq1[eq1['sa'] == a].score.mean() for a in eq1['sa']]
-eq2_sa_means = [eq2[eq2['sa'] == a].score.mean() for a in eq2['sa']]
-sse = sum((eq1.score - eq1_sa_means)**2) +sum((eq2.score - eq2_sa_means)**2)
+eq1_sa_means = [eq1[eq1['sa'] == a]['score'].mean() for a in eq1['sa']]
+eq2_sa_means = [eq2[eq2['sa'] == a]['score'].mean() for a in eq2['sa']]
+sse = sum((eq1['score'] - eq1_sa_means)**2) +sum((eq2['score'] - eq2_sa_means)**2)
 
 ssab = sstotal-ssa-ssb-sse                                         
 
@@ -94,3 +96,15 @@ model = ols(formula, data).fit()
 aov_table = anova_lm(model, typ=2)
 
 print(aov_table)
+
+'''Problem 2'''
+# The data ‘virtual training.csv’ examines the effects of different trainig methods a procedure to launch
+# a lifeboat (column 1, 1=lecture/materials, 2=monitor/keyboard, 3=head monitor display/joypad,
+# 4=head monitor display/wearables). The results are included in column 2 and are a measurement
+# of the post-test procedural knowledge score.
+# Construct an anova table to determine if the method of instruction had an effect on the procedural
+# knowledge score. Perform a multiple comparison of means using Bonferroni method (6 t-tests with
+# shared α) and Tukey.
+df_vt = pd.read_csv("virtual_training.csv", sep = ',')
+df_vt.columns = ['effect', 'result']
+print(df_vt.head())
