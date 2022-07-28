2153769b3c9cec34e9074efcf2c52d2c39d3b3ff
DanielFawcett1 <>
2022-07-28 17:08:24 +0000

    midterm thru p2

--- DASC512/midterm.py
+++ DASC512/midterm.py
@@ -1,3 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.stats as stats
+import seaborn as sns
+from pathlib import Path
+from statsmodels.stats import power
+import statsmodels.api as sm
+from statsmodels.stats import weightstats as stests
+
 '''Problem 1'''
 # Suppose that the four inspectors @film factory are supposed 
 # to stamp the expiration date on each package of film @EO the assembly line. 
@@ -7,26 +17,39 @@
 # Pat, who stamps 5% of the packages fails to stamp the expiration date once in every 200 packages. 
 # If a customer complains that her package of film does not show the expiration date, 
 # what is the probability that it was inspected by John?
-A: the produce is not marked
-B1: the produce is marked by John
-B2: the produce is marked by Tom
-B3: the produce is marked y Jeff
-B4: the produce is marked by Pat
 
-Applying the rule of elimination, the formula would be:
+# the pkg is marked by John
+prob_john = (.20)*(1/200)
+# print(prob_John)
+# the pkg is marked by Tom
+prob_tom = (.60)*(1/100)
+# the pkg is marked by Jeff
+prob_jeff = (.15)*(1/90)
+# the pkg is marked by Pat
+prob_pat =  (.05)*(1/200)
+# the pkg is not marked
+prob_no_exp_dt = prob_john + prob_tom + prob_jeff + prob_pat
+# print(prob_no_exp_dt)
+# No printed expiration date inspected by John
+prob_john_no_exp_dt = (prob_john/prob_no_exp_dt)
+# print('prob_no_exp_dt_John = %.4f' % prob_no_exp_dt_John)
 
-P (A) = P (B1) P (A|B1) + P (B2) P (A|B2) + P (B3) P (A|B3) + P (B4) P (A|B4)
+'''Problem 2'''
+A = (2, 4, 5, 7)
+B = (1, 3, 4, 7)
+C = (2, 2, 2, 5)
+sum_C = sum(C)
+# print(sum_C)
+human_error = C[3]
+# print(human_error)
+prob_he_int_c = human_error/sum_C
+# print('prob_he_int_c = %.4f' % prob_he_int_c)
 
-And the following are the probabilities:
+'''Problem 3'''
+df_faithful = pd.read_csv("faithful.csv", sep = ',')
+# print(df_faithful.head())
+eruptions = df_faithful['eruptions']
+waiting = df_faithful['waiting']
 
-P(B1)P(A|B1) = (.20)(1/200) = .001
-
-P(B2)P(A|B2) = (.60)(1/100) = .006
-
-P(B3)P(A|B3) = (.15)(1/90) = .00167
-
-P(B4)P(A|B4) = (.05)(1/200) = .00025
-
-The likelihood that the product is not imprinted is equal to .001 + .006 + .00167 + .00025 = .00892
-
-The probability that it was reviewed by John is = .001/.00892 = .1121
+plt.scatter(x=eruptions, y=waiting)
+plt.show()
