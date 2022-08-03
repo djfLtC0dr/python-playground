c76aed1005b3dd8f27a05a6ff83cae2e86fbd6f2
djfLtC0dr <fawcettdj1@gmail.com>
2022-08-03 17:20:10 -0400

    hw5 p3 complete

--- DASC512/hw5.py
+++ DASC512/hw5.py
@@ -154,7 +154,7 @@ UL=beta_1+tcrit*s/(SSxx**(1/2))
 LL=beta_1-tcrit*s/(SSxx**(1/2))
 
 print('We are', 100*(1-alpha), 
-'%confident that the true value of the slope is in the interval(', round(LL,2),',',round(UL,2),')')
+'%confident that the true value of the slope is in the interval(', round(LL,4),',',round(UL,4),')')
 
 # Prediction based on regression analysis
 x_day_stay = 4
@@ -262,7 +262,7 @@ UL=beta_1+tcrit*s/(SSxx**(1/2))
 LL=beta_1-tcrit*s/(SSxx**(1/2))
 
 print('We are', 100*(1-alpha), 
-'%confident that the true value of the slope is in the interval(', round(LL,2),',',round(UL,2),')')
+'%confident that the true value of the slope is in the interval(', round(LL,4),',',round(UL,4),')')
 
 # Prediction based on regression analysis
 x_obp = 0.40
@@ -361,7 +361,7 @@ UL=beta_1+tcrit*s/(SSxx**(1/2))
 LL=beta_1-tcrit*s/(SSxx**(1/2))
 
 print('We are', 100*(1-alpha), 
-'%confident that the true value of the slope is in the interval(', round(LL,2),',',round(UL,2),')')
+'%confident that the true value of the slope is in the interval(', round(LL,4),',',round(UL,4),')')
 
 # Prediction based on regression analysis
 x_obp = 0.40
@@ -390,3 +390,217 @@ ax.fill_between(x_pred, predints['mean_ci_lower'], predints['mean_ci_upper'], co
 ax.fill_between(x_pred, predints['obs_ci_lower'], predints['obs_ci_upper'], color='#888888', alpha=0.2)
 
 print('Difference in slope b/t w/ and w/o outliers is ' + str(1 - (beta_1_with_outlier/beta_1_without_outlier)))
+
+
+'''Problem 3 Run Diff using teamdata.csv'''
+#Read the data 
+df_team = pd.read_csv("teamdata.csv", sep = ',')
+# print(df_team.head())
+# clean-up unnecessary data
+df_team.drop(df_team.columns[df_team.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
+# print(df_team.head())
+# setup our dataframe for x,y data
+df_team['run_diff'] = df_team['R'] - df_team['RA'] # R−RA
+df_team['win_pct'] = df_team['W'] / (df_team['W'] + df_team['L']) # W/(W + L)
+# print(df_team.head())
+x = df_team['run_diff']
+y = df_team['win_pct']
+alpha = 0.05
+
+# Calculate xy stats
+x_mean, x_sum, y_mean, y_sum, xy, xx, yy, sum_xy, sum_xx, n = calc_xy_stats(x, y)
+
+# Applying functions to our data & print out the results:
+B0, B1, reg_line = linear_regression(x, y)
+print('Regression Line: ', reg_line)
+R = corr_coef(x, y)
+print('Correlation Coef. (i.e. R): ', R)
+print('Goodness of Fit (i.e. R^2): ', R**2)
+
+fig, ax = plt.subplots(figsize=(8, 4))
+text = '''X Mean: {} Years
+Y Mean: ${}
+R: {}
+R^2: {}
+y = {} + {}X'''.format(round(x.mean(), 2), 
+                       round(y.mean(), 2), 
+                       round(R, 4), 
+                       round(R**2, 4),
+                       round(B0, 3),
+                       round(B1, 3))
+# these are matplotlib.patch.Patch properties
+props = dict(boxstyle='round', facecolor='grey', alpha=0.2, pad=1)
+# place a text box in upper left in axes coords
+ax.text(0.05, 0.95, text, transform=ax.transAxes, fontsize=10,
+        verticalalignment='top', bbox=props)
+ax.scatter(x, y, alpha=0.75, s=0.5, linewidths=1, edgecolor='black',c='green')
+fig.suptitle('How Run Differential Affects Winning Pct')
+plt.xlabel('Run Diff', fontsize=15)
+plt.ylabel('Winning Pct', fontsize=15)
+plt.plot(x, B0 + B1*x, c='r', linewidth=1, alpha=alpha)
+ax.grid(True)
+# fig.savefig('hospital.png', dpi=300)
+
+# Calculate betas and fitted line
+SSxy, SSxx, beta_1, beta_0, fitted = calc_betas_fitted(sum_xy, x_sum, y_sum, n, sum_xx, y_mean, x_mean)
+
+# Create a plot with the fitted line
+x_pred = np.linspace(x.min(), x.max(), 50)
+y_pred=fitted(x_pred)
+ax.plot(x_pred, y_pred, '-', color='blue', linewidth=2)
+# fig.savefig('propellantfit.png', dpi=300)
+
+#More calculatsions, this time for error
+SSyy, SSE, MSE, s = calc_error(yy, y_sum, n, beta_1, SSxy)
+
+# Calc our coefficients
+r, r2, r2_alt = calc_coeffs(SSxy, SSxx, SSyy, SSE)
+
+# Calc our tstats
+tstat_r, pval = calc_tstats(r, n)
+print('tstat = ', tstat_r)
+print('pval = ', pval)
+
+#Confidence interval around Beta and t-test for Beta
+tstat=beta_1/(s/(SSxx**(1/2)))
+tcrit=stats.t.ppf(1-alpha/2,n-2)
+UL=beta_1+tcrit*s/(SSxx**(1/2))
+LL=beta_1-tcrit*s/(SSxx**(1/2))
+
+print('We are', 100*(1-alpha), 
+'%confident that the true value of the slope is in the interval(', round(LL,2),',',round(UL,2),')')
+
+# Prediction based on regression analysis
+x_run_diff = 300
+baseball_model_2_predict = predict(B0, B1, x_run_diff)
+print('Win Pct based on 300 Run Diff: ', baseball_model_2_predict)
+
+#output Model Summary
+df_rundiff_winpct=pd.DataFrame({'rundiff':x,'winpct':y})
+model_baseball_2=smf.ols('winpct~rundiff',df_rundiff_winpct).fit()
+print(model_baseball_2.summary2(alpha=alpha))
+beta_0 = model_baseball_2.params[0]
+beta_1 = model_baseball_2.params[1]
+fitted = lambda xx: beta_0 + beta_1*xx
+
+# Create a plot with the fitted line
+x_pred = np.linspace(x.min(), x.max(), 50)
+y_pred=fitted(x_pred)
+ax.plot(x_pred, y_pred, '-', color='blue', linewidth=2)
+# fig.savefig('propellantfit.png', dpi=300)
+
+#calculate prediction intervals
+df_pred = pd.DataFrame({'rundiff': x_pred,'winpct': y_pred})
+prediction=model_baseball_2.get_prediction(df_pred)
+predints=prediction.summary_frame(alpha=0.1)
+
+ax.fill_between(x_pred, predints['mean_ci_lower'], predints['mean_ci_upper'], color='#888888', alpha=0.6)
+ax.fill_between(x_pred, predints['obs_ci_lower'], predints['obs_ci_upper'], color='#888888', alpha=0.2)
+
+
+#***********************************************************************
+# Examine Regression w/ Pythagorean Expectation => R**2 / (R**2 + RA**2)
+#***********************************************************************
+df_team['py_exp'] = df_team['R']**2 / (df_team['R']**2 + df_team['RA']**2)
+# print(df_team.head())
+x = df_team['py_exp']
+y = df_team['win_pct']
+alpha = 0.05
+
+# Calculate xy stats
+x_mean, x_sum, y_mean, y_sum, xy, xx, yy, sum_xy, sum_xx, n = calc_xy_stats(x, y)
+
+# Applying functions to our data & print out the results:
+B0, B1, reg_line = linear_regression(x, y)
+print('Regression Line: ', reg_line)
+R = corr_coef(x, y)
+print('Correlation Coef. (i.e. R): ', R)
+print('Goodness of Fit (i.e. R^2): ', R**2)
+
+fig, ax = plt.subplots(figsize=(8, 4))
+text = '''X Mean: {} Years
+Y Mean: ${}
+R: {}
+R^2: {}
+y = {} + {}X'''.format(round(x.mean(), 2), 
+                       round(y.mean(), 2), 
+                       round(R, 4), 
+                       round(R**2, 4),
+                       round(B0, 3),
+                       round(B1, 3))
+# these are matplotlib.patch.Patch properties
+props = dict(boxstyle='round', facecolor='grey', alpha=0.2, pad=1)
+# place a text box in upper left in axes coords
+ax.text(0.05, 0.95, text, transform=ax.transAxes, fontsize=10,
+        verticalalignment='top', bbox=props)
+ax.scatter(x, y, alpha=0.75, s=0.5, linewidths=1, edgecolor='black',c='green')
+fig.suptitle('How Pythagorean Expectation Affects Winning Pct')
+plt.xlabel('Pythagorean Exp', fontsize=15)
+plt.ylabel('Winning Pct', fontsize=15)
+plt.plot(x, B0 + B1*x, c='r', linewidth=1, alpha=alpha)
+ax.grid(True)
+# fig.savefig('hospital.png', dpi=300)
+
+# Calculate betas and fitted line
+SSxy, SSxx, beta_1, beta_0, fitted = calc_betas_fitted(sum_xy, x_sum, y_sum, n, sum_xx, y_mean, x_mean)
+
+# Create a plot with the fitted line
+x_pred = np.linspace(x.min(), x.max(), 50)
+y_pred=fitted(x_pred)
+ax.plot(x_pred, y_pred, '-', color='blue', linewidth=2)
+# fig.savefig('propellantfit.png', dpi=300)
+
+#More calculatsions, this time for error
+SSyy, SSE, MSE, s = calc_error(yy, y_sum, n, beta_1, SSxy)
+
+# Calc our coefficients
+r, r2, r2_alt = calc_coeffs(SSxy, SSxx, SSyy, SSE)
+
+# Calc our tstats
+tstat_r, pval = calc_tstats(r, n)
+print('tstat = ', tstat_r)
+print('pval = ', pval)
+
+#Confidence interval around Beta and t-test for Beta
+tstat=beta_1/(s/(SSxx**(1/2)))
+tcrit=stats.t.ppf(1-alpha/2,n-2)
+UL=beta_1+tcrit*s/(SSxx**(1/2))
+LL=beta_1-tcrit*s/(SSxx**(1/2))
+
+print('We are', 100*(1-alpha), 
+'%confident that the true value of the slope is in the interval(', round(LL,2),',',round(UL,2),')')
+
+# Prediction based on regression analysis
+R = 927
+RA = 627
+x_py_exp = R**2 / (R**2 + RA**2)
+baseball_model_3_predict = predict(B0, B1, x_py_exp)
+print('Win Pct based on Pythagorean Exp: ', baseball_model_3_predict)
+
+#output Model Summary
+df_pyexp_winpct=pd.DataFrame({'py_exp':x,'winpct':y})
+model_baseball_3=smf.ols('winpct~py_exp',df_pyexp_winpct).fit()
+print(model_baseball_3.summary2(alpha=alpha))
+beta_0 = model_baseball_3.params[0]
+beta_1 = model_baseball_3.params[1]
+fitted = lambda xx: beta_0 + beta_1*xx
+
+# Create a plot with the fitted line
+x_pred = np.linspace(x.min(), x.max(), 50)
+y_pred=fitted(x_pred)
+ax.plot(x_pred, y_pred, '-', color='blue', linewidth=2)
+# fig.savefig('propellantfit.png', dpi=300)
+
+#calculate prediction intervals
+df_pred = pd.DataFrame({'py_exp': x_pred,'winpct': y_pred})
+prediction=model_baseball_3.get_prediction(df_pred)
+predints=prediction.summary_frame(alpha=0.1)
+
+ax.fill_between(x_pred, predints['mean_ci_lower'], predints['mean_ci_upper'], color='#888888', alpha=0.6)
+ax.fill_between(x_pred, predints['obs_ci_lower'], predints['obs_ci_upper'], color='#888888', alpha=0.2)
+
+
+win_loss_sea = 116 / (116 + 46)
+
+print('Difference in prediction for baseball model 2 to actual W/L is  ' + str(1 - (win_loss_sea/baseball_model_2_predict)))
+print('Difference in prediction for baseball model 3 to actual W/L is  ' + str(1 - (win_loss_sea/baseball_model_3_predict)))
