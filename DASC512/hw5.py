532e493409f0a6115c51a8bfd25ff3a60b78b343
djfLtC0dr <fawcettdj1@gmail.com>
2022-08-03 11:03:16 -0400

    hw5 p1 complete

--- DASC512/hw5.py
+++ DASC512/hw5.py
@@ -7,29 +7,88 @@ from sklearn.linear_model import LinearRegression  #used for the example of leas
 from sklearn.linear_model import RANSACRegressor #used for example of 'something else' line only
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
+import warnings
+
+#ignore by message
+warnings.filterwarnings("ignore", message="kurtosistest only valid for n>=20")
 
 '''Problem 1'''
-# We would like to determine if the average length of stay can predict the average hospital charge 
+# We would like to determine if the average length of stay (x) can predict the average hospital charge (y)
 # and use it to make a prediction for the charge when a patient stays 4 days. Perform a regression 
 # analysis to determine the answer to this question, use α = 0.10 when required.
 
 #Read the data and assign 'x' and 'y'
 hospital=pd.read_csv('hospital_data.csv')
-hospital = hospital.rename(columns = {'Average Charge': 'avg_charge', 'Average Length of Stay (days)': 'avg_len_stay'})
+hospital = hospital.rename(columns = {'Average Charge': 'avg_charge', 
+                            'Average Length of Stay (days)': 'avg_len_stay'})
 x = hospital['avg_len_stay']
 y = hospital['avg_charge']
+alpha = 0.10
 
-#Reset the plot so that I can use it as a blank slate to add to later
+#Calculate some basic values for use later
+def linear_regression(x, y):     
+    N = len(x)
+    x_mean = np.mean(x)
+    y_mean = np.mean(y)
+    
+    B1_num = ((x - x_mean) * (y - y_mean)).sum()
+    B1_den = ((x - x_mean)**2).sum()
+    B1 = B1_num / B1_den
+    
+    B0 = y_mean - (B1 * x_mean)
+    
+    reg_line = 'y = {} + {}β'.format(B0, round(B1, 4))
+    
+    return (B0, B1, reg_line)
+
+def corr_coef(x, y):
+    N = len(x)
+    
+    num = (N * (x*y).sum()) - (x.sum() * y.sum())
+    den = np.sqrt((N * (x**2).sum() - x.sum()**2) * (N * (y**2).sum() - y.sum()**2))
+    R = num / den
+    return R
+
+def predict(B0, B1, new_x):
+    y = B0 + B1 * new_x
+    return y    
+
+# Applying functions to our data & print out the results:
+B0, B1, reg_line = linear_regression(x, y)
+print('Regression Line: ', reg_line)
+R = corr_coef(x, y)
+print('Correlation Coef. (i.e. R): ', R)
+print('Goodness of Fit (i.e. R^2): ', R**2)
+
+# Prediction based on regression analysis
+x_day_stay = 4
+print('4-Day Stay Prediction: ', predict(B0, B1, x_day_stay))
+
 fig, ax = plt.subplots(figsize=(8, 4))
-ax.scatter(x, y, alpha=0.9, color='orchid')
-fig.suptitle('Hospital predictions')
-plt.xlabel("Average Length of Stay (days)")
-plt.ylabel("Average Charge")
-fig.tight_layout(pad=2); 
+text = '''X Mean: {} Years
+Y Mean: ${}
+R: {}
+R^2: {}
+y = {} + {}X'''.format(round(x.mean(), 2), 
+                       round(y.mean(), 2), 
+                       round(R, 4), 
+                       round(R**2, 4),
+                       round(B0, 3),
+                       round(B1, 3))
+# these are matplotlib.patch.Patch properties
+props = dict(boxstyle='round', facecolor='grey', alpha=0.2, pad=1)
+# place a text box in upper left in axes coords
+ax.text(0.05, 0.95, text, transform=ax.transAxes, fontsize=10,
+        verticalalignment='top', bbox=props)
+ax.scatter(x, y, alpha=0.75, s=30, linewidths=1, edgecolor='black',c='green')
+fig.suptitle('How Hospital Length Affects Charges')
+plt.xlabel('Average Length of Stay (days)', fontsize=15)
+plt.ylabel('Average Charge', fontsize=15)
+plt.plot(x, B0 + B1*x, c='r', linewidth=1, alpha=alpha) # solid_capstyle='round')
 ax.grid(True)
 # fig.savefig('hospital.png', dpi=300)
 
-#Calculate some basic values for use later
+
 xbar=np.mean(x)
 xsum=sum(x)
 ybar=np.mean(y)
@@ -50,7 +109,7 @@ fitted = lambda xx: beta0 + beta1*xx
 #Create a plot with the fitted line
 x_pred = np.linspace(x.min(), x.max(), 50)
 y_pred=fitted(x_pred)
-ax.plot(x_pred, y_pred, '-', color='darkblue', linewidth=2)
+ax.plot(x_pred, y_pred, '-', color='blue', linewidth=2)
 # fig.savefig('propellantfit.png', dpi=300)
 
 #More calculatsions, this time for error
@@ -58,8 +117,6 @@ SSyy=sum(np.multiply(y,y))-ysum**2/n
 SSE=SSyy-beta1*SSxy
 MSE=SSE/(n-2)
 s=MSE**(1/2)
-
-alpha = 0.1
 
 r = SSxy/(SSxx*SSyy)**(1/2)
 r2 = r**2
@@ -72,11 +129,12 @@ print('pval = ', pval)
 
 #Confidence interval around Beta and t-test for Beta
 tstat=beta1/(s/(SSxx**(1/2)))
-tcrit=stats.t.ppf(1-0.05/2,n-2)
+tcrit=stats.t.ppf(1-alpha/2,n-2)
 UL=beta1+tcrit*s/(SSxx**(1/2))
 LL=beta1-tcrit*s/(SSxx**(1/2))
 
-print('We are', 100*(1-alpha), '%confident that the true value of the slope is in the interval', round(LL,2),',',round(UL,2),')')
+print('We are', 100*(1-alpha), 
+'%confident that the true value of the slope is in the interval(', round(LL,2),',',round(UL,2),')')
 
 #Estimation and Prediction Interval calculations
 se_est = lambda x: s * np.sqrt(  1./n + (x-xbar)**2/SSxx)
@@ -94,8 +152,8 @@ LowerPredEx=fitted(x_p)-abs(stats.t.ppf(1-alpha/2,n-2)*se_pred(x_p))
 upper = y_pred + abs(stats.t.ppf(1-alpha/2,n-2)*se_est(x_pred))
 lower = y_pred - abs(stats.t.ppf(1-alpha/2,n-2)*se_est(x_pred))
 ax.fill_between(x_pred, lower, upper, color='#888888', alpha=0.6)
-upperp = y_pred + abs(stats.t.ppf(1-0.05/2,n-2)*se_pred(x_pred))
-lowerp = y_pred - abs(stats.t.ppf(1-0.05/2,n-2)*se_pred(x_pred))
+upperp = y_pred + abs(stats.t.ppf(1-alpha/2,n-2)*se_pred(x_pred))
+lowerp = y_pred - abs(stats.t.ppf(1-alpha/2,n-2)*se_pred(x_pred))
 ax.fill_between(x_pred, lowerp, upperp, color='#888888', alpha=0.2)
 # fig.savefig('CIex.png', dpi=300)
 
@@ -111,3 +169,6 @@ predints=prediction.summary_frame(alpha=alpha)
 
 anova_table = sm.stats.anova_lm(model1)
 anova_table[:-1]['sum_sq']/sum(anova_table['sum_sq'])
+
+
+'''Problem 2'''
